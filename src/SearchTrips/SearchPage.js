6cf42b81fa602abf544c2c61fb6c662454e8fa6c
kimhoken <kimhk441@naver.com>
2026-04-01 08:49:01 +0900

    sub3

--- src/SearchTrips/SearchPage.js
+++ src/SearchTrips/SearchPage.js
@@ -1,10 +1,11 @@
 import react from "react";
+import { Link } from "react-router-dom";
 
 const SearchPage=()=>{
 
     return(
         <div>
-            test
+            
         </div>
     )
 }
