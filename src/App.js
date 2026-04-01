9a5313ef66701c2a6932cefb456c779e6d2a9cca
0425bhs <0425bhs@gmail.com>
2026-04-01 17:19:49 +0900

    sub1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import ReservationList from './pages/MyTrips/ReservationList';
+import Favorites from './pages/MyTrips/Favorites';
+import TravelTips from './pages/Board/TravelTips';
+import ReservationForm from './pages/MyTrips/ReservationForm';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [reservations,setReservations]=useState([])
+// 로컬스토리지에서 예약 불러오기
+  useEffect(() => {
+    const saved = localStorage.getItem("reservations")
+    if(saved){
+      setReservations(JSON.parse(saved))
+    }
+  },[])
+  //예약 변경 시 로컬스토리지에 저장
+  useEffect(() => {
+    localStorage.setItem("reservations", JSON.stringify(reservations))},[reservations])
+ 
+  //새예약 추가
+  const addReservation = (reservation) => {
+    setReservations(prev => [...prev, reservation])
+  }
+  //예약삭제
+  const deleteReservation = (id) => {
+    setReservations(prev => prev.filter(item => item.id !== id))
+  }
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <nav style={{ padding: "10px", borderBottom: "1px solid #000" }}>
+        {/* <Link to="/">홈</Link> */}
+        <Link to="/reservations" style={{ margin: "10px" }}>내 여행</Link>
+        <Link to="/favorites" style={{ margin: "10px" }}>즐겨찾기</Link>
+        <Link to="/tips" style={{ margin: "10px" }}>여행 팁</Link>
+        <Link to="/reserve" style={{ marginRight: "10px" }}>예약하기</Link>
+      </nav>  
+
+      <Routes>
+        <Route path="/" element={<ReservationList data={reservations} onDelete={deleteReservation}/>} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/tips" element={<TravelTips />} />
+        <Route path="/reserve" element={<ReservationForm addReservation={addReservation}/>} />
+        <Route path='/reservations' element={<ReservationList data={reservations} onDelete={deleteReservation}/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/MyTrips/ReservationList.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+// import { getFavorites, getReservations } from "../../api/tripApi";
+import ReservationCard from "../../components/ReservationCard";
+
+export default function ReservationList({data,onDelete}){
+
+    if (!data) return <p>로딩중...</p>;
+
+    return(
+        <div>
+            <h2>내 예약</h2>
+
+            {
+                data.length===0 ? (<p>예약이 존재하지 않습니다.</p>) : 
+                (
+                    data.map((item)=>(
+                        <div>
+                            <h3>{item.title}</h3>
+                            <p>여행지: {item.destination}</p>
+                            <p>{item.startDate} ~ {item.endDate}</p>
+                            <p>일정: {item.days}</p>
+                            <button onClick={() => onDelete(item.id)}>삭제</button>
+                        </div>
+                    ))
+                )
+            }
+        </div>
+    )
+}
