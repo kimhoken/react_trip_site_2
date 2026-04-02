c8e8c1522ba3737588b430dfea7895754e3f7829
heeu4825-star <heeu4825@gmail.com>
2026-04-02 15:17:25 +0900

    sub4

---
+++ src/pages/PackagesMain.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./PackagesMain.css";
+
+const PackagesMain=()=>{
+
+    
+    return(
+        <div className="packages-card">
+            <div className="packages-ch">
+                
+
+            </div>
+
+            
+
+        </div>
+    )
+
+
+}
+
+export default PackagesMain
+
