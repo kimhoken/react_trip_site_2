385cbe3ae4413d6daf6a44ecb5f436bf2501c333
kimhoken <kimhk441@naver.com>
2026-04-02 17:39:38 +0900

    master

--- src/App.js
+++ src/App.js
@@ -110,14 +110,9 @@ function App() {
         <Route path='/SearchTrips' element={<SearchPage />} />
         <Route path="/SearchTrips/PackageInfo/:id" element={<Packageinfo />} />
         <Route path='/PopularDestinations' element={<PopularDestinations />} />
-
-        {/* 여기 마이페이지.. 세팅 해야함 */}
-        <Route path="/MyTrips" element={<ReservationList data={reservations} onDelete={deleteReservation}/>} /> 
-        <Route path="/MyTrips/favorites" element={<Favorites />} />
-        <Route path="/Board/tips" element={<TravelTips />} />
-        <Route path="MyTrips/reserve" element={<ReservationForm addReservation={addReservation}/>} />
-        <Route path='MyTrips/reservations' element={<ReservationList data={reservations} onDelete={deleteReservation}/>}/>
-        <Route path='/MyTrips/mypage' element={<MyPage/>} />
+     
+      
+        
       </Routes>
     </BrowserRouter>
   );
