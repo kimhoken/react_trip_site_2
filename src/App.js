80881835b301391ef27eb7bcabdd00d419bee04f
0425bhs <0425bhs@gmail.com>
2026-04-03 16:18:35 +0900

    sub1

--- src/App.js
+++ src/App.js
@@ -79,19 +79,11 @@ function App() {
                   <Link className='nav-link' to="/Board">Community</Link>
                 </li>
 
-                {/* <li className='nav-item mytrips'>
-                  <Link className='nav-link' to="/MyTrips">My Trips</Link>
-                </li> */}
-
                 <li className='nav-item mytrips'>
-                  <span className='nav-link'>My Trips</span>
-                  <ul >
-                    <li><Link className='nav-link' to="/MyTrips/reserve">예약하기</Link></li>
-                    <li><Link className='nav-link' to="/MyTrips/reservations">예약/취소내역</Link></li>
-                    <li><Link className='nav-link' to="/MyTrips/favorites">찜목록</Link></li>
-                    <li><Link className='nav-link' to="/MyTrips/mypage">마이페이지</Link></li>
-                  </ul>
+                  <Link className='nav-link' to="/MyTrips">My Trips</Link>
                 </li>
+
+                
 
 
 
