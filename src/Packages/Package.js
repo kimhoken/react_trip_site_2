6bc420ad0da35ff2d96cd4a5c678552b485ba461
kimhoken <kimhk441@naver.com>
2026-04-07 22:43:57 +0900

    sub3

--- src/Packages/Package.js
+++ src/Packages/Package.js
@@ -5,8 +5,8 @@ import './Package.css';
 const Package =()=>{
     return(
         <div className="package-main">
-            <Link to={'/Packages/DomesticPage'}>국내 패키지</Link>
-            <Link to={'/Packages/OverseasPage'}>해외 패키지</Link>            
+            <Link to={'/Packages/DomesticPage'} className="package-headerlink">국내 패키지</Link>
+            <Link to={'/Packages/OverseasPage'} className="package-headerlink">해외 패키지</Link>            
             <Outlet/>
             
         </div>
