87c0e34ae4538804122ab22991215614726f5786
kimhoken <kimhk441@naver.com>
2026-04-08 20:23:16 +0900

    sub3

--- src/SearchTrips/DomesticPage.js
+++ src/SearchTrips/DomesticPage.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { DomesticPackageList } from "../Packages/DomesticPackageList";
 import { Link } from "react-router-dom";
 import './DomesticPage.css';
 
 const DomesticPage = () => {
+    const [category,setCategory]=useState('');
+    const [active,setActive]=useState('');
 
     const showlist = () => {
         let list = DomesticPackageList;
 
         list = list.filter((item) => item.rating >= 4.0 && item.reviewCount > 20
-            && item.type == 'domestic')
+            && (!category || item.contient == category))
 
         {
 
@@ -22,12 +24,10 @@ const DomesticPage = () => {
                             </div>
                             <div>
                                 <h3>{item.title}</h3>
-                                <div>평점: {item.rating}</div>
-                                <div>리뷰수: {item.reviewCount}</div>
+                                <div> {item.price}</div>
+                                <div><span>★</span> {item.rating}({item.reviewCount})</div>
                             </div>
-                            <div>
-                                <div>가격: {item.price}</div>
-                            </div>
+                            
                         </Link>
                     </li>
 
@@ -36,11 +36,28 @@ const DomesticPage = () => {
 
         }
     }
+    const region = [
+        { locate: '전체', contient: '' }, { locate: '수도권', contient: 'suduck' }, { locate: '강원', contient: 'kang' },
+        { locate: '충청', contient: 'chung' },{locate: '전라', contient:'jeon'},{ locate:'경상', contient:'gyeong'},{locate:'제주',contient:'je'}
+    ];
+
+    const showcategory = () => {
+        {
+            return region.map((i) => {
+                return (
+                    <p onClick={() => { setCategory(i.contient); setActive(i.contient) }}
+                        className={active == i.contient ? "active" : ""}
+                    >{i.locate}</p>
+                )
+            })
+        }
+    }
 
 
     return (
         <div className="Domestic-main">
             <h2>국내 여행 패키지</h2>
+            {showcategory()}
             <ul>
                 {showlist()}
             </ul>
