127465b50f2e33f8ac3ad749c460b041219fb20c
heeu4825-star <heeu4825@gmail.com>
2026-04-09 16:56:15 +0900

    sub4

--- src/pages/MyTrips/MyPage.js
+++ src/pages/MyTrips/MyPage.js
@@ -26,57 +26,69 @@ const Mypage = () => {
 
   return (
     <div>
-      <div>
-        <h1>내 정보관리</h1>
+      <div className="mypage-info">
+        <h1 className="mypage-title">내 정보관리</h1>
       </div>
 
       <div>
-        <div>
-          <h2>{loginUser.name} 님</h2>
-          <p>아이디: {loginUser.id}</p>
-          <p>비밀번호 : {loginUser.pw}</p>
-          <p>생년월일 : {loginUser.birth}</p>
-          <p>전화번호: {loginUser.phone}</p>
-          <p>이메일: {loginUser.email}</p>
+        <div className="mypage-info-list">
+          <h2 className="mypage-user-name">{loginUser.name} 님</h2>
+          <p className="mypage-user-info">아이디: {loginUser.id}</p>
+          <p className="mypage-user-info">생년월일 : {loginUser.birth}</p>
+          <p className="mypage-user-info">전화번호: {loginUser.phone}</p>
+          <p className="mypage-user-info">이메일: {loginUser.email}</p>
+          
+          <div className="management-information-logout">
+            <button
+                type="button"
+                className="management-information-logout-btn"
+                onClick={handleLogout}
+                >
+                로그아웃
+            </button>
+            </div>
+        </div>
+        
+      </div>
+
+      <div className="mypage-section">
+        <p  className="mypage-menu-toggle" 
+             onClick={() => setMenu(menu === "term" ? "" : "term")}>
+          약관 확인 
+        <span className="mypage-menu-arrow">▷</span>
+        </p>
+
+        <div className="mypage-term-box">
+            {menu === "term" && (
+            <div className="mypage-term-txtlist">
+                <p className="mypage-term-text">
+                • 마케팅 수신 동의 (이메일/SMS) 시 <br />
+                상품 혜택 및 이벤트 정보를
+                <br />
+                받으실 수 있습니다.
+                </p>
+                <p className="mypage-term-text">
+                • 예약/결제 정보 안내 등은
+                <br />
+                마케팅 수신 동의와 <br />
+                상관없이 발송됩니다.
+                </p>
+                {/* <p className="mypage-term-text">
+                    • 서비스 이용을 위해 약관 및 개인정보 <br/>
+                     처리방침을 확인하고 이에 동의합니다.
+                </p> */}
+
+            </div>
+            )}
+        </div>
+
+        <div className="mypage-link-box">
+            <Link to="/ForgotPassword" className="mypage-password-link">
+                    비밀번호 변경</Link>
         </div>
       </div>
 
-      <div>
-        <p onClick={() => setMenu(menu === "term" ? "" : "term")}>
-          약관 확인 ▷
-        </p>
-
-        {menu === "term" && (
-          <div>
-            <p>
-              • 마케팅 수신 동의 (이메일/SMS) 시 <br />
-              상품 혜택 및 이벤트 정보를
-              <br />
-              받으실 수 있습니다.
-            </p>
-            <p>
-              • 예약/결제 정보 안내 등은
-              <br />
-              마케팅 수신 동의와 <br />
-              상관없이 발송됩니다.
-            </p>
-          </div>
-        )}
-      </div>
-
-      <div>
-        <Link to="/ForgotPassword">• 비밀번호 변경</Link>
-      </div>
-
-      <div>
-        <button
-          type="button"
-          className="user-link signup logout"
-          onClick={handleLogout}
-        >
-          로그아웃
-        </button>
-      </div>
+      
     </div>
   );
 };
